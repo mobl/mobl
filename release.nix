d2784ab57762affd1514fa3907b6812717ff5bde
chrismelman <Chrismelman@bitbucket.org>
2012-02-21 21:19:15 +0100

    aparently the file is already there. So therfore a force to overwrite the file

--- release.nix
+++ release.nix
@@ -145,7 +145,7 @@ let
         cp -Rv ${mobl} mobl
         chmod -R a+w mobl
         mkdir -p mobl/utils
-        cp -v `find $eclipse -name StrategoMix.def` mobl/utils/
+        cp -vf `find $eclipse -name StrategoMix.def` mobl/utils/
         export LOCALCLASSPATH="utils/js.jar"
       '';
     };
