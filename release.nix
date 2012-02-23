40d0bc14ea8c1872e999b83af42bf24df84cb9c3
chrismelman <Chrismelman@bitbucket.org>
2012-02-23 17:16:12 +0100

    hoppefully fixes stuf

--- release.nix
+++ release.nix
@@ -138,7 +138,7 @@ let
     updatesite = import "${hydraConfig}/spoofax-fun.nix" {
       inherit pkgs;
       name = "mobl";
-      version = "0.3.9.${toString mobl.revCount}";
+      version = "0.3.99.${toString mobl.revCount}";
       src = moblPlugin;
       buildInputs = [pkgs.strategoPackages.sdf];
       preConfigure = ''
