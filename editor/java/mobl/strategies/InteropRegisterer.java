91da22e6f3f045ff557fdac52dc0ad431c4da5ce
chrismelman <Chrismelman@hotmail.com>
2012-03-14 15:02:12 +0100

    moved buildoncloud from mobl.registries

--- editor/java/mobl/strategies/InteropRegisterer.java
+++ editor/java/mobl/strategies/InteropRegisterer.java
@@ -1,7 +1,13 @@
 package mobl.strategies;
 
+import mobl.strategies.compress_0_0;
+import mobl.strategies.deltree_0_0;
+import mobl.strategies.path_to_dataurl_0_0;
+
 import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
+
+
 
 /**
  * Helper class for {@link java_strategy_0_0}.
@@ -10,6 +16,6 @@ public class InteropRegisterer extends JavaInteropRegisterer {
 
     public InteropRegisterer() {
         super(new Strategy[] { deltree_0_0.instance, uglify_0_0.instance, compress_0_0.instance,
-                path_to_dataurl_0_0.instance,build$On$Cloud_0_0.instance });
+                path_to_dataurl_0_0.instance});
     }
 }
