1ea9f2bba6dbcb4529626caa30e6c422337613bb
chrismelman <Chrismelman@hotmail.com>
2012-03-19 01:28:17 +0100

    another structure

--- release.nix
+++ release.nix
@@ -118,10 +118,10 @@ let
         ulimit -s unlimited
         mkdir -p utils
         cp -v ${aster} utils/aster.jar
-        cp -v ${make_permissive}/lib/make_permissive.jar utils/make_permissive.jar
+        cp -v ${make_permissive} utils/make_permissive.jar
         cp -v ${strategoxt} utils/strategoxt.jar
-        cp -v ${sdf2imp}/lib/sdf2imp.jar utils/sdf2imp.jar
-        cp -v ${strategomix}/lib/StrategoMix.def utils/StrategoMix.def
+        cp -v ${sdf2imp} utils/sdf2imp.jar
+        cp -v ${strategomix} utils/StrategoMix.def
         ensureDir $out/bin
       '';
        meta.maintainers = maindevelopers;
